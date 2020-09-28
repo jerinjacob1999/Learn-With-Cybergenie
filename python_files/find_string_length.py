89793f3f0b476310a79078ee121156a663df8370
Jerin Jacob <123jerinjacob007@gmail.com>
2020-09-28 03:24:21 -0700

    Python file
    find string length from input string

---
+++ python_files/find_string_length.py
@@ -0,0 +1,3 @@
+input_text=input("Type a stringg to find number of charactars : ") #Creates a variable to accept text from user
+print("The input string contains ", length(input_text), "charactars including spaces") #print the chararcter count
+#Fix: instead of "length()" in the above line it must be "len"()
